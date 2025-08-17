77eb23e62e0644535c32c76f45b061bf14900fa0
prabhatdby <58321293+prabhatdby@users.noreply.github.com>
2025-08-17 11:36:20 +0530

    last commit

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,6 +1,6 @@
 import { Testimonial } from "@/types/testimonial";
 import SectionTitle from "../Common/SectionTitle";
-import SingleTestimonial from "./SingleTestimonial";
+//import SingleTestimonial from "./SingleTestimonial";
 
 const testimonialData: Testimonial[] = [
   {
