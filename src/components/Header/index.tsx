77c9a778ea3a59e1601a2aa1e981039548c62c85
prabhatdby <58321293+prabhatdby@users.noreply.github.com>
2025-08-17 11:26:22 +0530

    first commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -145,9 +145,9 @@ const Header = () => {
                                 openIndex === index ? "block" : "hidden"
                               }`}
                             >
-                              {menuItem.submenu.map((submenuItem, index) => (
+                              {menuItem.submenu &&  menuItem.submenu.map((submenuItem, index) => (
                                 <Link
-                                  href={submenuItem.path}
+                                  href={submenuItem.path ?? "#"}
                                   key={index}
                                   className="text-dark hover:text-primary block rounded-sm py-2.5 text-sm lg:px-3 dark:text-white/70 dark:hover:text-white"
                                 >
